e5cbfc4ff0fa6b37e5a9c9674238d22c70a071b0
marco p <miamarine84@yahoo.com>
2020-08-25 21:44:10 -0400

    code working just need to get badges information

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 // const prompt = require('prompt');
 const inquirer = require('inquirer');
+const { parse } = require('path');
 
 // prompt.start();
 let finalOutput = []
@@ -16,12 +17,12 @@ const promptChoices = [
   'Questions']
 
 
-console.log('Hi, welcome to Node Pizza');
+console.log('This is the README.md creator please take your time and fill this out.');
 
 var questions = [
   {
     type: 'input',
-    name: '## Title',
+    name: 'Title',
     message: 'What is the title of your page?',
     default: false,
   },
@@ -34,13 +35,25 @@ var questions = [
   {
     type: 'input',
     name: 'Installation',
-    message: 'No installation instructions necessary',
+    message: 'Installation instructions',
     default: false,
   },
   {
     type: 'input',
     name: 'Usage',
     message: 'Usage Information',
+    default: 'No usage information necessary',
+  },
+  {
+    type: 'input',
+    name: 'Contribution',
+    message: 'Details if contributing',
+    default: 'No usage information necessary',
+  },
+  {
+    type: 'input',
+    name: 'Testing',
+    message: 'Testing information',
     default: 'No usage information necessary',
   },
   {
@@ -57,51 +70,97 @@ var questions = [
     name: 'Github',
     message: 'What is your github username?',
     filter: function (answers) {
+      if(answers){
       return `https://github.com/${answers}`
+      }else{
+        return ''
+      }
+
     }
   },
   {
     type: 'input',
-    name: 'E-Mail',
+    name: 'Email',
     message: 'What is your email address',
-    default: 'none',
+    default: false,
     filter: function (answers) {
-      return `${answers} Please feel free to also go to my github leave me a message or contact me with any questions you may have`
+      if(answers){
+        return `${answers} This is my e-mail please feel free to contact me through here also go to my github leave me a message and contact me with any questions you may have`
+      }else{
+        return '' 
+      }
     }
   },
 ];
 
 inquirer.prompt(questions).then((answers) => {
-  console.log('\nOrder receipt:');
-  console.log('the big test', answers)
+  // console.log('\nOrder receipt:');
+  //console.log('the big test', answers)
 
   console.log(JSON.stringify(answers, null, '  '));
   let fileJson = JSON.stringify(answers, null, '  ')
-  console.log('this is the fileJson output', fileJson)
+  // console.log('this is the fileJson output', fileJson)
   let parsedObj = JSON.parse(fileJson)
-  console.log('parsed',parsedObj)
-  let docTitle = parsedObj.pageTitle
-  console.log(docTitle)
+  console.log('parsed', parsedObj)
 
-   let finalOutput = [];
 
-  for (const property in parsedObj) {
-    finalOutput +=
-      `
-    ${property}:
-    ${parsedObj[property]}
-     `
-     console.log( `${property}:
-      ${parsedObj[property]}`)
-  }
+  //  let finalOutput = [];
 
-console.log(finalOutput)
+  // for (const property in parsedObj) {
+  //   finalOutput +=
+  //     `
+  //   ${property}:
+  //   ${parsedObj[property]}
+  //    `
+  //    console.log( `${property}:
+  //     ${parsedObj[property]}`)
+  // }
+  console.log(parsedObj.Email)
 
-  fs.writeFile(parsedObj.docTitle + '.md', finalOutput, function (err) {
-    //console.log('this is in the write file portion' + finalOutput)
-    if (err) throw err;
 
-  });
+
+
+  fs.writeFile(parsedObj.Title + '.md',
+
+    `
+## Title 
+${parsedObj.Title}
+
+## Description
+
+${parsedObj.Description} 
+## Table Of Contents 
+* [Title](#Title)
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributing](#Contributing)
+* [Testing](#Testing)
+* [Questions](#Questions)
+
+${parsedObj.Installation}
+## Usage 
+
+${parsedObj.Usage}
+# Contributing 
+
+${parsedObj.Contribution}
+## Testing
+
+${parsedObj.Testing}
+## Questions
+
+${parsedObj.Github}
+${parsedObj.Email}    
+
+    `
+
+
+    , function (err) {
+      //console.log('this is in the write file portion' + finalOutput)
+      if (err) throw err;
+
+    });
 
 });
 
